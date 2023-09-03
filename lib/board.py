f15b855a3e83ff4eb116e5d38464f031e66d6b15
ayrton <ayrton.parkinson1@gmail.com>
2023-09-03 09:30:09 -0400

    Add self.guess and self.remove_guess

--- lib/board.py
+++ lib/board.py
@@ -3,12 +3,35 @@ import copy
 class Sudoku:
     """Class representation of sudoku board. Used to check whether guess is valid and keep track of guesses."""
 
+    # Takes 2D array of board and creates board, initial board, and board properties instance variables
     def __init__(self, board):
-        # Create board, initial board, and board properties instance variables
         self.board = board
         self.initial_board = [[v for v in row] for row in board]
         self.properties = self.create_board_properties(board)
     
+    # Insert value on board and update board properties - Return bool if successful
+    def guess(self, row, col, value):
+        if self.initial_board[row][col] > 0:
+            return False
+        
+        if self.board[row][col] > 0:
+            self.remove_guess(row, col)
+        
+        self.board[row][col] = value
+        self.properties['row'][row][value] = True
+        self.properties['col'][col][value] = True
+        self.properties['box'][3 * (row // 3) + (col // 3)][value] = True
+
+    def remove_guess(self, row, col):
+        if self.initial_board[row][col] > 0:
+            return False
+        
+        value = self.board[row][col]
+        self.board[row][col] = 0
+        self.properties['row'][row][value] = False
+        self.properties['col'][col][value] = False
+        self.properties['box'][3 * (row // 3) + (col // 3)][value] = False
+
     # Takes board and returns hash containing properties of board for easy validation of row, col, and box values
     def create_board_properties(self, board):
         properties = {}
@@ -37,6 +60,14 @@ if __name__ == '__main__':
         [0, 0, 0, 0, 8, 0, 0, 7, 9]
     ]
     sudoku = Sudoku(board)
-    for row in sudoku.properties['box']:
+    sudoku.guess(0, 2, 9)
+    print(sudoku.properties['row'][0])
+
+    for row in sudoku.board:
         print(row)
 
+    sudoku.remove_guess(0, 2)
+    print(sudoku.properties['row'][0])
+
+    for row in sudoku.board:
+        print(row)
